73fb377e10ad6d8cfba5b7ffa5295f6151494af5
Joni Wong Angkasa <jwangkasa@gmail.com>
2025-03-11 23:14:00 +0800

    Updated with Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import ClarityScript from "./components/ClarityScript";
 import GoogleTagManager from "./components/GoogleTagManager";
 import GoogleAnalytics from "./components/GoogleAnalytics";
-import Script from 'next/script';
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,10 +27,15 @@ export const metadata: Metadata = {
   description: "The next generation of AI for event marketplaces and portals",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         {/* Google Tag Manager and Analytics Scripts */}
         <Script
           strategy="afterInteractive"
@@ -47,21 +52,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
-        
+
         {/* Render the Clarity Script */}
         <ClarityScript />
-        
+
         {/* Render Google Tag Manager (including noscript) */}
         <GoogleTagManager />
-        
+
         {/* Render Google Analytics (including noscript) */}
         <GoogleAnalytics />
       </head>
-    
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${parisienne.variable} antialiased`}
       >
-      {children}
+        {children}
       </body>
     </html>
   );
